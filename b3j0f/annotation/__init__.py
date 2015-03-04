26c33f777fb520400b083de4b2d6bad1c7ba84fa
mrbozzo <jlabejof@yahoo.fr>
2015-03-04 21:27:35 +0100

    :update: add beta version

--- b3j0f/annotation/__init__.py
+++ b3j0f/annotation/__init__.py
@@ -24,7 +24,7 @@
 # SOFTWARE.
 # --------------------------------------------------------------------
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
 __all__ = ['Annotation', 'StopPropagation', 'RoutineAnnotation']
 
