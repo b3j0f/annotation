ef2057fd1dfda00d00c1f8f0ce48c799e97cafa1
b3j0f <jonathan.labejof@gmail.com>
2015-11-09 01:07:00 +0100

    :add: support for python3.5 and revision version

--- setup.py
+++ setup.py
@@ -91,7 +91,8 @@ setup(
         'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
     ],
     test_suite='b3j0f',
     keywords=KEYWORDS
